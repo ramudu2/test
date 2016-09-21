afebf0a5f8d6d0862aba69285d612183bb44d261
ramudu2 <singanamallaramudu2@gmail.com>
2016-09-21 12:34:38 +0530

    Add files via upload

---
+++ ArrayTest_1.java
@@ -0,0 +1,14 @@
+public class ArrayTest_1
+{
+public static void main(String args[])
+{
+int[]numbers;
+numbers=new int[5];
+numbers[0]=10;
+numbers[1]=20;
+numbers[2]=30;
+System.out.println("elements at index0--"+numbers[0]);
+System.out.println("elements at index1--"+numbers[1]);
+System.out.println("elements at index2--"+numbers[2]);
+}
+}
